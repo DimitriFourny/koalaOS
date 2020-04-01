eda88107246a852f4d4d4c0b35f23e7ac2c46667
DimitriFourny <fourny.d@gmail.com>
2020-04-01 16:28:35 +0200

    Using KEY_UP defines

--- kern/Keyboard.cpp
+++ kern/Keyboard.cpp
@@ -6,16 +6,21 @@
 bool Keyboard::left_shift_ = false;
 bool Keyboard::right_shift_ = false;
 
+#define KEY_UP_BIT 0x80
+#define KEY_CLR_UP_BIT(key) (key & ~KEY_UP_BIT)
+#define KEY_IS_UP(key) (key & KEY_UP_BIT)
+#define KEY_IS_DOWN(key) (!KEY_IS_UP(key))
+
 void Keyboard::ShowCharacter(unsigned char character) {
-  switch (character & ~0x80) {
+  switch (KEY_CLR_UP_BIT(character)) {
     case 0x29:
-      left_shift_ = !(character & 0x80);
+      left_shift_ = KEY_IS_DOWN(character);
       break;
     case 0x35:
-      right_shift_ = !(character & 0x80);
+      right_shift_ = KEY_IS_DOWN(character);
       break;
     default:
-      if (character < 0x80) {
+      if (KEY_IS_DOWN(character)) {
         const bool is_uppercase = left_shift_ || right_shift_;
         Screen::printCharacter(KEYBOARD[character * 4 + is_uppercase],
                                SCREEN_LIGHT_MAGENTA);
